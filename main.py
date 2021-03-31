f8a9c769a99d931ff6861d746da2f134364aa0b9
aaronamk <amklein@email.wm.edu>
2021-03-31 04:37:49 -0400

    Search proof of concept (needs work).

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+
 
 nextbook = Flask(__name__)
 
-@nextbook.route("/")
+
+@nextbook.route("/", methods = ['POST', 'GET'])
 def search():
-    return "Search: "
+    if request.method == "POST":
+        test = request.form["phrase"]
+        return redirect(url_for('results', filter = "class", phrase = test))
+    else:
+        test = request.args.get("phrase")
+        return redirect(url_for('results', filter = "class", phrase = test))
+
 
 @nextbook.route("/results/<filter>/<phrase>")
 def results(filter, phrase):
     return f"Search by {filter}: {phrase}"
 
+
 @nextbook.route("/class-list")
 def class_list():
     return "full list"
+
 
 @nextbook.route("/add-book")
 def add_book():
     return "add a book"
 
+
 @nextbook.route("/about")
 def about():
     return "We are cool bros!"
+
 
 if __name__ == "__main__":
     nextbook.run()
